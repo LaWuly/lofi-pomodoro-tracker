93198c6dbce11f6fddaa8331b05ad9f95c3aa3f5
Wuly <isabella.tarlao@gmail.com>
2025-10-04 14:06:06 +0200

    chore: configura linting, prettier e alias TS

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["react", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
