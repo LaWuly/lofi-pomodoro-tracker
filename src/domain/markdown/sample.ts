1ae2289be07cc6ee1740b6b5333391ff18010026
Wuly <isabella.tarlao@gmail.com>
2025-10-15 13:23:48 +0200

    feat(markdown): aggiunge SAMPLE_MD

---
+++ src/domain/markdown/sample.ts
@@ -0,0 +1,22 @@
+// src/domain/markdown/sample.ts
+export const DEFAULT_MD = `# Hello Markdown!
+
+> *Scrivi a sinistra • vedi l'anteprima a destra*
+
+## Titoli
+- H1, H2, H3…
+
+## Formattazione
+**grassetto**, _corsivo_, \`inline code\`
+
+## Blocco di codice
+\`\`\`js
+console.log('Hello World!')
+\`\`\`
+
+## Link
+[FreeCodeCamp](https://www.freecodecamp.org)
+
+## Citazione
+> “Markdown è semplice ma potente.”
+`
